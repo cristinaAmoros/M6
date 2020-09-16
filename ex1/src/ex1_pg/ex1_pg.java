f8e0e7c3b3728a6cb1b9cd7d653106d4bccf6c41
cristinaAmoros <60181969+cristinaAmoros@users.noreply.github.com>
2020-09-16 12:02:07 +0200

    sumar

--- ex1/src/ex1_pg/ex1_pg.java
+++ ex1/src/ex1_pg/ex1_pg.java
@@ -1,11 +1,32 @@
 package ex1_pg;
-
+import java.util.Scanner;
 public class ex1_pg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("hola");
- 
+ System.out.println("hola, anem a sumar joder");
+ 	Scanner sc = new Scanner(System.in);
+ 	int n1=0, n2=0, r=0 ;
+ 	System.out.println("fica primer num");
+ 	n1 = sc.nextInt();
+ 	System.out.println("fica segon num");
+ 	n2 = sc.nextInt();
+ 	r= n1+n2;
+ 	System.out.println("el resultat es " + r);
+ 	
+ 	if(r>0 && r<=5) {
+ 		System.out.println("primer if");
+ 	}
+ 	else if(r>5 && r<=8) {
+ 		System.out.println("segon if");
+ 	}
+ 	else if(r>8) {
+ 		System.out.println("tercer if");
+ 	}
+ 	else {
+ 		System.out.println("Error");
+ 	}
+ 	
 	}
 
 }
